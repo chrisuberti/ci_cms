643d4733d9079d3cd9ce5ffd237852140a65af93
Chris Uberti <cuberti87@gmail.com>
2016-12-22 16:35:15 +0000

    Successfully moved to new workspace with working phpmyadmin, Need to fix some bootstrap references or something but DB and CI both appear to be working well

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = "https://uber-ci-cms-v2-cuberti87.c9users.io/";
+$config['base_url'] = "https://ubercms-cuberti87.c9users.io/";
 
 /*
 |--------------------------------------------------------------------------
